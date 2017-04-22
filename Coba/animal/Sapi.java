827b83b74a60c3d6c77a34c3fd222054cbcf6190
Amuzdalifa <a.muzdalifa@gmail.com>
2017-04-23 01:52:57 +0700

    package animal

---
+++ Coba/animal/Sapi.java
@@ -0,0 +1,8 @@
+/**
+ * Created by nim_13515090 on 23/04/17.
+ */
+public class Sapi extends Animal {
+    public Sapi(int id, int absis, int ordinat, String behaviour) {
+        super(id, 6, absis, ordinat, 'S', behaviour);
+    }
+}
