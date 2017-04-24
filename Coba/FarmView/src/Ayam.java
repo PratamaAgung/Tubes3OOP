5c9f2945e7115c61ac87fa61bb6ec2daaa43bd89
Amuzdalifa <a.muzdalifa@gmail.com>
2017-04-24 16:18:06 +0700

    View

--- Coba/FarmView/src/Ayam.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by nim_13515090 on 23/04/17.
- */
-public class Ayam extends Animal {
-
-    public Ayam(int id, int absis, int ordinat, String behaviour) {
-        super(id, 1, absis, ordinat, 'A', behaviour);
-    }
-}
--- Coba/FarmView/src/Bebek.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by nim_13515090 on 23/04/17.
- */
-public class Bebek extends Animal {
-    public Bebek(int id, int absis, int ordinat, String behaviour) {
-        super(id, 2, absis, ordinat, 'B', behaviour);
-    }
-}
