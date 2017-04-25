419f6a4ecdd5410689ffc6144272b3d2797240c0
Amuzdalifa <a.muzdalifa@gmail.com>
2017-04-25 23:21:30 +0700

    new 2 Animal Controller

--- FarmView/src/animal/AnimalController.java
+++ FarmView/src/animal/AnimalController.java
@@ -1,6 +1,7 @@
 package animal;
 
 import cell.Cell;
+import cell.CellController;
 import resizer.PictureResizer;
 
 import javax.swing.*;
@@ -15,37 +16,49 @@ public class AnimalController implements Runnable, PictureResizer {
     private JTextPane[][] map;
     private Random rand;
     private int nbAnimal;
-    private Cell[][] farm;
+    private CellController cellController;
+    private boolean[] animalstatus;
 
-    public AnimalController(JTextPane[][] map, Cell[][] cell) {
+    public AnimalController(JTextPane[][] map, CellController cellController) {
         this.map = map;
         animal = new Animal[15];
         rand = new Random();
-        farm = cell;
+        this.cellController = cellController;
         animal[0] = new Ayam(1, 3, 6, "Kukuruyuuk");
         animal[1] = new Ayam(2, 7, 18, "I'm an aves but i can't fly");
         animal[2] = new Ayam(3, 10, 15, "Why did i cross the road?");
-        animal[3] = new Bebek(4, 3, 10, "Kwek kwek kwek");
+        animal[3] = new Bebek(4, 2, 10, "Kwek kwek kwek");
         animal[4] = new Bebek(5, 11, 6, "An aves that can swim");
         animal[5] = new Bebek(6, 5, 14, "Mickey mouse is my friend");
         animal[6] = new Domba(7, 7, 6, "People use my wool");
         animal[7] = new Domba(8, 18, 9, "Baa Baa Black Sheep");
         animal[8] = new Kelinci(9, 9, 11, "I hop a lot");
-        animal[9] = new Kelinci(10, 17, 17, "People say i love carrot");
+        animal[9] = new Kelinci(10, 14, 17, "People say i love carrot");
         animal[10] = new Kelinci(11, 9, 9, "i am a fluffy ball of fur");
-        animal[11] = new Kuda(12, 16, 2, "if i have wings, people will call me pegasus");
-        animal[12] = new Kuda(13, 15, 8, "I can be used for transportation");
+        animal[11] = new Kuda(12, 16, 5, "if i have wings, people will call me pegasus");
+        animal[12] = new Kuda(13, 15, 7, "I can be used for transportation");
         animal[13] = new Sapi(14, 9, 19, "People love my milk");
         animal[14] = new Sapi(15, 8, 8, "Moo");
         nbAnimal = 15;
         for (int i = 0; i < nbAnimal; i++) {
+            animalstatus[i] = true;
+            map[animal[i].getAbsis()][animal[i].getOrdinat()].setText("");
             map[animal[i].getAbsis()][animal[i].getOrdinat()].insertIcon(animal[i].getIcon());
+            cellController.getCell(animal[i].getAbsis(),animal[i].getOrdinat()).setAnimal(animal[i]);
         }
     }
 
     public void addAnimal(Animal anAnimal) {
         animal[nbAnimal] = anAnimal;
         nbAnimal++;
+    }
+
+    public boolean[] getAnimalstatus() {
+        return animalstatus;
+    }
+
+    public void setAnimalstatus(int id) {
+        animalstatus[id-1] = false;
     }
 
     public Animal getAnimalById(int id) {
@@ -70,51 +83,70 @@ public class AnimalController implements Runnable, PictureResizer {
         ImageIcon grass = resizePicture("data/grass.png", 25, 25);
         while (true) {
             for(int i = 0; i < nbAnimal; i++) {
-                int x = animal[i].getAbsis();
-                int y = animal[i].getOrdinat();
-                int start = rand.nextInt(4) + 1;
-                boolean found = false;
-                while (!found) {
-                    if (start == 1) {
-                        if (x - 1 >= 0 && farm[x-1][y].getId() == 0) {
-                            found = true;
-                            map[x][y].insertIcon(grass);
-                            x--;
-                            map[x][y].insertIcon(animal[i].getIcon());
-                        } else {
-                            start = start % 4 + 1;
-                        }
-                    } else if (start == 2) {
-                        if (y - 1 >= 0 && farm[x][y-1].getId() == 0) {
-                            found = true;
-                            map[x][y].insertIcon(grass);
-                            y--;
-                            map[x][y].insertIcon(animal[i].getIcon());
-                        } else {
-                            start = start % 4 + 1;
-                        }
-                    } else if (start == 3) {
-                        if (x + 1 < 20 && farm[x+1][y].getId() == 0) {
-                            found = true;
-                            map[x][y].insertIcon(grass);
-                            x++;
-                            map[x][y].insertIcon(animal[i].getIcon());
-                        } else {
-                            start = start % 4 + 1;
-                        }
-                    } else if (start == 4) {
-                        if (y + 1 < 20 && farm[x][y+1].getId() == 0) {
-                            found = true;
-                            map[x][y].insertIcon(grass);
-                            y++;
-                            map[x][y].insertIcon(animal[i].getIcon());
-                        } else {
-                            start = start % 4 + 1;
+                if (animalstatus[i]) {
+                    int x = animal[i].getAbsis();
+                    int y = animal[i].getOrdinat();
+                    int start = rand.nextInt(4) + 1;
+                    boolean found = false;
+                    while (!found) {
+                        if (start == 1) {
+                            if (x - 1 >= 0 && (cellController.getCell(x-1, y).getId() == 0 || cellController.getCell(x-1, y).getId() == animal[i].getIdcage())) {
+                                found = true;
+                                map[x][y].setText("");
+                                map[x][y].insertIcon(grass);
+                                cellController.getCell(x, y).eraseAnimal();
+                                x--;
+                                map[x][y].setText("");
+                                map[x][y].insertIcon(animal[i].getIcon());
+                                cellController.getCell(x, y).setAnimal(animal[i]);
+                            } else {
+                                start = start % 4 + 1;
+                            }
+                        } else if (start == 2) {
+                            if (y - 1 >= 0 && (cellController.getCell(x, y-1).getId() == 0 || cellController.getCell(x, y-1).getId() == animal[i].getIdcage())) {
+                                found = true;
+                                map[x][y].setText("");
+                                map[x][y].insertIcon(grass);
+                                cellController.getCell(x, y).eraseAnimal();
+                                y--;
+                                map[x][y].setText("");
+                                map[x][y].insertIcon(animal[i].getIcon());
+                                cellController.getCell(x, y).setAnimal(animal[i]);
+                            } else {
+                                start = start % 4 + 1;
+                            }
+                        } else if (start == 3) {
+                            if (x + 1 < 20 && (cellController.getCell(x+1, y).getId() == 0 || cellController.getCell(x+1, y).getId() == animal[i].getIdcage())) {
+                                found = true;
+                                map[x][y].setText("");
+                                map[x][y].insertIcon(grass);
+                                cellController.getCell(x, y).eraseAnimal();
+                                x++;
+                                map[x][y].setText("");
+                                map[x][y].insertIcon(animal[i].getIcon());
+                                cellController.getCell(x, y).setAnimal(animal[i]);
+                            } else {
+                                start = start % 4 + 1;
+                            }
+                        } else if (start == 4) {
+                            //System.out.println(x + " " + y + " " + farm[x][y+1].getId());
+                            if ((y + 1 < 20 ) && (cellController.getCell(x, y+1).getId() == 0 || cellController.getCell(x, y + 1).getId() == animal[i].getIdcage())) {
+                                found = true;
+                                map[x][y].setText("");
+                                map[x][y].insertIcon(grass);
+                                cellController.getCell(x, y).eraseAnimal();
+                                y++;
+                                map[x][y].setText("");
+                                map[x][y].insertIcon(animal[i].getIcon());
+                                cellController.getCell(x, y).setAnimal(animal[i]);
+                            } else {
+                                start = start % 4 + 1;
+                            }
                         }
                     }
+                    animal[i].setAbsis(x);
+                    animal[i].setOrdinat(y);
                 }
-                animal[i].setAbsis(x);
-                animal[i].setOrdinat(y);
             }
             try {
                 Thread.sleep(800);
