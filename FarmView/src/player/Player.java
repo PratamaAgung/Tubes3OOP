c176236f4ed266bd89c48829b2d00679b5ffb55a
Iftitakhul Zakiah <iftitakhulzakiyah@gmail.com>
2017-04-26 14:55:17 +0700

    player lolos checkstyle

--- FarmView/src/player/Player.java
+++ FarmView/src/player/Player.java
@@ -1,69 +1,142 @@
 package player;
 
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import resizer.PictureResizer;
 
-import javax.swing.*;
-import java.awt.*;
-
 /**
- * Created by iftitakhul on 18/04/17.
+ * Kelas Player digunakan untuk merepresentasikan player dalam games.
+ * @author iftitakhul on 18/04/17.
  */
-//import java.lang.*;
-
 public class Player implements PictureResizer {
-    private String name;
-    private int score;
-    private int absis, ordinat;
-    private ImageIcon avatar;
+  private String name;
+  private int score;
+  private int absis;
+  private int ordinat;
+  private ImageIcon avatar;
 
-    public Player(String name, int absis, int ordinat, int isGirl) {
-        this.name = name;
-        this.score = 0;
-        this.absis = absis;
-        this.ordinat = ordinat;
-        if (isGirl == 1) {
-            this.avatar = resizePicture("data/FarmGirl.png",25,25);
-        } else   {
-            this.avatar = resizePicture("data/FarmBoy.png",25,25);
-        }
-
+  /**
+   * Constructor Player.
+   * I.S : -.
+   * F.S : Menghasilkan player sesuai dengan parameter input.
+   * @param name merepresentasikan nama player dari input.
+   * @param absis merepresentasikan posisi awal player pada absis.
+   * @param ordinat merepresentasikan posisi awal player pada ordinat.
+   * @param isGirl digunakan untuk membedakan apakah player wanita atau pria.
+   */
+  public Player(String name, int absis, int ordinat, int isGirl) {
+    this.name = name;
+    this.score = 0;
+    this.absis = absis;
+    this.ordinat = ordinat;
+    if (isGirl == 1) {
+      this.avatar = resizePicture("data/FarmGirl.png",25,25);
+    } else {
+      this.avatar = resizePicture("data/FarmBoy.png",25,25);
     }
+  }
 
-    public ImageIcon getAvatar() {
-        return avatar;
-    }
+  /**
+   * Getter avatar.
+   * I.S : Player terdefinisi.
+   * F.S : Mengembalikan avatar player dalam bentuk ImageIcon.
+   */
+  public ImageIcon getAvatar() {
+    return avatar;
+  }
 
-    public int getAbsis() {return  absis;}
+  /**
+   * Getter Absis.
+   * I.S : Player terdefinisi.
+   * F.S : Mengembalikan posisi player sekarang pada absis.
+   */
+  public int getAbsis() {
+    return  absis;
+  }
 
-    public int getOrdinat() {return  ordinat;}
+  /**
+   * Getter Ordinat.
+   * I.S : Player terdefinisi.
+   * F.S : Mengembalikan posisi player sekarang pada ordinat.
+   */
+  public int getOrdinat() {
+    return  ordinat;
+  }
 
-    public String getName() {
-        return name;
-    }
+  /**
+   * Getter Nama.
+   * I.S : Player terdefinisi.
+   * F.S : Mengembalikan nama player.
+   */
+  public String getName() {
+    return name;
+  }
 
-    public int getScore() {
-        return score;
-    }
+  /**
+   * Getter Score.
+   * I.S : Player terdefinisi.
+   * F.S : Mengembalikan nilai score dari player.
+   */
+  public int getScore() {
+    return score;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  /**
+   * Setter Name.
+   * I.S : Player terdefinisi.
+   * F.S : name berhasil dimasukkan ke atribut name.
+   * @param name yang dimasukkan ke atribut.
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setScore(int score) {
-        this.score = score;
-    }
+  /**
+   * Setter Score.
+   * I.S : Player terdefinisi.
+   * F.S : score berhasil dimasukkan ke atribut score.
+   * @param score yang dimasukkan ke atribut.
+   */
+  public void setScore(int score) {
+    this.score = score;
+  }
 
-    public void setAbsis(int absis) {this.absis = absis; }
+  /**
+   * Setter Ordinat.
+   * I.S : Player terdefinisi.
+   * F.S : absis berhasil dimasukkan ke atribut absis.
+   * @param absis yang dimasukkan ke atribut.
+   */
+  public void setAbsis(int absis) {
+    this.absis = absis;
+  }
 
-    public void setOrdinat(int ordinat) {this.ordinat = ordinat; }
+  /**
+   * Setter Ordinat.
+   * I.S : Player terdefinisi.
+   * F.S : ordinat berhasil dimasukkan ke atribut ordinat.
+   * @param ordinat yang dimasukkan ke atribut.
+   */
+  public void setOrdinat(int ordinat) {
+    this.ordinat = ordinat;
+  }
 
-    @Override
-    public ImageIcon resizePicture(String path, int height, int width) {
-        ImageIcon picture = new ImageIcon(path);
-        Image image = picture.getImage();
-        Image resizeimage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        ImageIcon picturetemp = new ImageIcon(resizeimage);
-        return  picturetemp;
-    }
+  /**
+   * Implementasi method resizePicture dari interface PictureResizer.
+   * I.S : path merupakan relative path ke gambar yang ingin dimuat, height dan width
+   *     adalah ukuran gambar setelah resize.
+   * F.S : mengembalikan imageIcon dari gambar yang telah di-resize.
+   * @param path path relative dari file gambar.
+   * @param height tinggi dari gambar hasil resize.
+   * @param width lebar gambar setelah resize.
+   */
+  @Override
+  public ImageIcon resizePicture(String path, int height, int width) {
+    ImageIcon picture = new ImageIcon(path);
+    Image image = picture.getImage();
+    Image resizeimage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+    ImageIcon picturetemp = new ImageIcon(resizeimage);
+    return  picturetemp;
+  }
 
 }
--- FarmView/src/player/PlayerController.java
+++ FarmView/src/player/PlayerController.java
@@ -1,104 +1,124 @@
 package player;
 
 import cell.CellController;
-import resizer.PictureResizer;
-
-import java.awt.*;
+import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import resizer.PictureResizer;
 
 /**
- * Created by pratamaagung on 21/04/17.
+ * Kelas yang digunakan untuk mmengatur player dalam games.
+ * @author pratamaagung and iftitakhul on 21/04/17.
  */
 public class PlayerController extends KeyAdapter implements PictureResizer {
-    private JTextPane [][] map;
-    private Player player;
-    private JLabel name;
-    private JLabel score;
-    private CellController cellController;
+  private JTextPane [][] map;
+  private Player player;
+  private JLabel name;
+  private JLabel score;
+  private CellController cellController;
 
-    public PlayerController(JTextPane[][] map, int x, int y, int isGirl, String nama, CellController cellController){
-        this.map = map;
-        player = new Player(nama,x,y,isGirl);
-        this.cellController = cellController;
-        if (isGirl == 1) {
-            map[y][x].setText("");
-            map[y][x].insertIcon(resizePicture("data/FarmGirl.png", 25, 25));
-        } else {
-            map[y][x].setText("");
-            map[y][x].insertIcon(resizePicture("data/FarmBoy.png", 25, 25));
-        }
+  /**
+   * Constructor PlayerController.
+   * I.S : -.
+   * F.S : PlayerController terbentuk sesuai dengan parameter input.
+   * @param map merepresentasikan peta yang digunakan.
+   * @param x merupakan letak awal player pada absis.
+   * @param y merupakan letak awal player pada ordinat.
+   * @param isGirl merepresentasikan apakah player wanita atau pria.
+   * @param nama merupakan nama player yang diinput.
+   * @param cellController digunakan untuk sumber informasi tentang cell.
+   */
+  public PlayerController(JTextPane[][] map, int x, int y, int isGirl,
+      String nama, CellController cellController) {
+    this.map = map;
+    player = new Player(nama,x,y,isGirl);
+    this.cellController = cellController;
+    if (isGirl == 1) {
+      map[y][x].setText("");
+      map[y][x].insertIcon(resizePicture("data/FarmGirl.png", 25, 25));
+    } else {
+      map[y][x].setText("");
+      map[y][x].insertIcon(resizePicture("data/FarmBoy.png", 25, 25));
     }
+  }
 
-    @Override
-    public void keyPressed(KeyEvent event){
-        int keyCode = event.getKeyCode();
-        ImageIcon icon = resizePicture("data/Bebek.png",25,25);
-        ImageIcon grass = resizePicture("data/grass.png", 25, 25);
-        int x = player.getAbsis();
-        int y = player.getOrdinat();
-        if (keyCode == 38) {
-            if(y - 1 >= 0 && (cellController.getCell(x,y - 1).getId() == 0 || cellController.getCell(x,y - 1).getId() == -1)) {
-                map[x][y].setText("");
-                map[x][y].insertIcon(grass);
-                player.setOrdinat(y-1);
-                map[x][y-1].setText("");
-                map[x][y-1].insertIcon(player.getAvatar());
-            }
-        } else if (keyCode == 40) {
-            if(y + 1 < 20 && (cellController.getCell(x,y + 1).getId() == 0 || cellController.getCell(x,y + 1).getId() == -1)) {
-                map[x][y].setText("");
-                map[x][y].insertIcon(grass);
-                player.setOrdinat(y + 1);
-                map[x][y+1].setText("");
-                map[x][y+1].insertIcon(player.getAvatar());
-            }
-        } else if (keyCode == 37) {
-            //System.out.println((x-1) + " " + y+ " " +cellController.getCell(x-1,y).getId());
-            if(x - 1 >= 0 && (cellController.getCell(x - 1, y).getId() == 0 || cellController.getCell(x - 1,y).getId() == -1)) {
-                map[x][y].setText("");
-                map[x][y].insertIcon(grass);
-                player.setAbsis(x - 1);
-                map[x-1][y].setText("");
-                map[x-1][y].insertIcon(player.getAvatar());
-            }
-        } else if (keyCode == 39) {
-            //System.out.println((x+1) + " " + y+ " " +cellController.getCell(x+1,y).getId());
-            if (x + 1 < 20 && (cellController.getCell(x + 1,y).getId() == 0 || cellController.getCell(x + 1,y).getId() == -1)) {
-                map[x][y].setText("");
-                map[x][y].insertIcon(grass);
-                player.setAbsis(x + 1);
-                map[x+1][y].setText("");
-                map[x+1][y].insertIcon(player.getAvatar());
-            }
-        }
+  /**
+   * I.S : Event terdefinisi, yaitu panah ke atas, bawah, kanan, dan kiri.
+   * F.S : Player bergerak sesuai dengan keyevent yang dimasukkan.
+   */
+  @Override
+  public void keyPressed(KeyEvent event) {
+    int keyCode = event.getKeyCode();
+    ImageIcon icon = resizePicture("data/Bebek.png",25,25);
+    ImageIcon grass = resizePicture("data/grass.png", 25, 25);
+    int x = player.getAbsis();
+    int y = player.getOrdinat();
+    if (keyCode == 38) {
+      if (y - 1 >= 0 && (cellController.getCell(x,y - 1).getId() == 0
+          || cellController.getCell(x,y - 1).getId() == -1)) {
+        map[x][y].setText("");
+        map[x][y].insertIcon(grass);
+        player.setOrdinat(y - 1);
+        map[x][y - 1].setText("");
+        map[x][y - 1].insertIcon(player.getAvatar());
+      }
+    } else if (keyCode == 40) {
+      if (y + 1 < 20 && (cellController.getCell(x,y + 1).getId() == 0
+          || cellController.getCell(x,y + 1).getId() == -1)) {
+        map[x][y].setText("");
+        map[x][y].insertIcon(grass);
+        player.setOrdinat(y + 1);
+        map[x][y + 1].setText("");
+        map[x][y + 1].insertIcon(player.getAvatar());
+      }
+    } else if (keyCode == 37) {
+      if (x - 1 >= 0 && (cellController.getCell(x - 1, y).getId() == 0
+          || cellController.getCell(x - 1,y).getId() == -1)) {
+        map[x][y].setText("");
+        map[x][y].insertIcon(grass);
+        player.setAbsis(x - 1);
+        map[x - 1][y].setText("");
+        map[x - 1][y].insertIcon(player.getAvatar());
+      }
+    } else if (keyCode == 39) {
+      if (x + 1 < 20 && (cellController.getCell(x + 1,y).getId() == 0
+          || cellController.getCell(x + 1,y).getId() == -1)) {
+        map[x][y].setText("");
+        map[x][y].insertIcon(grass);
+        player.setAbsis(x + 1);
+        map[x + 1][y].setText("");
+        map[x + 1][y].insertIcon(player.getAvatar());
+      }
     }
+  }
 
-    @Override
-    public void keyReleased(KeyEvent event) {
+  /**
+   * Implementasi method resizePicture dari interface PictureResizer.
+   * I.S : path merupakan relative path ke gambar yang ingin dimuat, height dan width
+   *     adalah ukuran gambar setelah resize.
+   * F.S : mengembalikan imageIcon dari gambar yang telah di-resize.
+   * @param path path relative dari file gambar.
+   * @param height tinggi dari gambar hasil resize.
+   * @param width lebar gambar setelah resize.
+   */
+  @Override
+  public ImageIcon resizePicture(String path, int height, int width) {
+    ImageIcon picture = new ImageIcon(path);
+    Image image = picture.getImage();
+    Image resizeimage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+    ImageIcon picturetemp = new ImageIcon(resizeimage);
+    return  picturetemp;
+  }
 
-    }
-
-    public void catchAnimal() {
-
-    }
-
-    public void releaseAnimal() {
-
-    }
-
-    @Override
-    public ImageIcon resizePicture(String path, int height, int width) {
-        ImageIcon picture = new ImageIcon(path);
-        Image image = picture.getImage();
-        Image resizeimage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        ImageIcon picturetemp = new ImageIcon(resizeimage);
-        return  picturetemp;
-    }
-
-    public Player getPlayer() {
-        return player;
-    }
-
+  /**
+   * Getter Player.
+   * I.S : Player terdefinisi.
+   * F.S. : mengambalikan atribut player.
+   */
+  public Player getPlayer() {
+    return player;
+  }
 }
